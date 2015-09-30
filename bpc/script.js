bd9c61d5e1a64964f9ea160f341a589ef1dfd3c3
Michael D. Mooring <mikeumus@gmail.com>
2015-09-30 10:24:11 +0000

    Finalizing first release of BPC sat-site.

--- bpc/script.js
+++ bpc/script.js
@@ -1,12 +1,31 @@
 $(function() {
     // http://stackoverflow.com/questions/18831568/jquery-onclick-not-firing-on-dynamically-inserted-html-elements
+    // All bpc-detail links
     $('a[href="#bpc-detail"]').on('click', function(){
-    //$('#hero-link').on('click', 'a',  function(){
         $('#bpc-detail-link').simulate('click'); 
-        console.log('clicked!!!!!')
     });
     
-    if($('#hero-link').data('clicked')) {
-        alert('yes');
-    }
+    // link to hidden updates page
+    //$('#updates-link').on('click', function(){
+    $('a[href="#updates"]').on('click', function(){
+        $('#updates').simulate('click'); 
+    });
+    
+    $('#tt2').on('click', function(){
+        $('#compete-link').simulate('click'); 
+    });
+    
+    $('#privacy-footer-link').on('click', function(){
+        $('#privacy-link').simulate('click'); 
+    });
+    
+    // Override Material Design Lite tab event and load meetup page
+    $('#attend-header-link').on('click', function() {
+       window.location.assign('http://www.meetup.com/NewSpace-NYC/events/225224233/'); 
+    });
+    // Override Material Design Lite tab event and load Space Fontier page
+    $('#bpc-rules-link').on('click', function() {
+       window.location.assign('http://newspacebpc.com/competition-rules/'); 
+    });
+
 });
